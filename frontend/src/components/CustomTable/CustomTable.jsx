daf653e5975f6254a2833383ff338efcf34834c0
wojciechwaleszczykttc <wojciech.waleszczyk@ttc.com>
2023-02-12 15:43:08 +0100

    MUSPP-8 add basic components to application

---
+++ frontend/src/components/CustomTable/CustomTable.jsx
@@ -0,0 +1,146 @@
+import Table from "react-bootstrap/Table";
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
+import "./CustomTable.css";
+
+function CustomTable() {
+    return (
+        <Tabs
+            defaultActiveKey="mathematics"
+            id="justify-tab-example"
+            className="mb-3 table__custom"
+            justify
+        >
+            <Tab eventKey="mathematics" title="Mathematics">
+                <Table striped bordered hover className="table">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Grades</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>5</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>4</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>3</td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </Tab>
+            <Tab eventKey="history" title="History">
+                <Table striped bordered hover className="table">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Grades</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>5</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>4</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>3</td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </Tab>
+            <Tab eventKey="physics" title="Physics">
+                <Table striped bordered hover className="table">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Grades</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>5</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>4</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>3</td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </Tab>
+            <Tab eventKey="geography" title="Geography">
+                <Table striped bordered hover className="table">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Grades</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>5</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>4</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>3</td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </Tab>
+        </Tabs>
+    );
+}
+
+export default CustomTable;
